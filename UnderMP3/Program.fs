729778a4631381022cbfdc14a065942efb31034b
pedablio <paulohrl96@gmail.com>
2018-01-29 14:55:11 -0200

    build folder

--- UnderMP3/Program.fs
+++ UnderMP3/Program.fs
@@ -2,8 +2,10 @@
 open System.IO
 
 let musicsFolder = "Musics"
+let buildFolder = "Tagged"
 
 // Create folder if not exists
 Directory.CreateDirectory musicsFolder |> ignore
+Directory.CreateDirectory buildFolder |> ignore
 
 Console.ReadKey() |> ignore
