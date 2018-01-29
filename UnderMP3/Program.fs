61c42933356f58ec4598a006f3b754d4c851c646
pedablio <paulohrl96@gmail.com>
2018-01-29 15:09:04 -0200

    getting .mp3 files

--- UnderMP3/Program.fs
+++ UnderMP3/Program.fs
@@ -8,4 +8,12 @@ let buildFolder = "Tagged"
 Directory.CreateDirectory musicsFolder |> ignore
 Directory.CreateDirectory buildFolder |> ignore
 
+// Get MP3 Files
+let files = 
+    Directory.GetDirectories musicsFolder
+    |> Array.collect Directory.GetFiles
+    |> Array.filter (fun s -> s.EndsWith (".mp3", StringComparison.OrdinalIgnoreCase))
+
+printfn "%A" files
+
 Console.ReadKey() |> ignore
